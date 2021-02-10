fb5f1fb8c1bb1e013228e054e72e04c447b5e5c8
“[Mateo <[lmateo@uninorte.edu.co]>
2021-02-10 22:45:32 +0100

    Using Hooks - creating AnimalDropdown and BreedDropdown. Also eslit-plugin-react-hooks was download and configure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams.js";
 
 const App = () => {
   return (
     <div id="something">
       <h1>Adopt Me!!</h1>
-      <Pet name="Fito" animal="Dog" breed="Afador" />
-      <Pet name="Martin Caballero" animal="Dog" breed="Beagle" />
-      <Pet name="Riko" animal="Dog" breed="Border Collie" />
+      <SearchParams />
     </div>
   );
 };
