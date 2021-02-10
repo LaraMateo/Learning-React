30b9ca36173780f68c608762843e0dac64686af7
“[Mateo <[lmateo@uninorte.edu.co]>
2021-02-10 12:51:12 +0100

    Using JSX instead of pure React

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "something" }, [
-    React.createElement("h1", {}, "Adopt Me!!"),
-    React.createElement(Pet, { name: "Fito", animal: "Dog", breed: "Afador" }),
-    React.createElement(Pet, {
-      name: "Martin",
-      animal: "Dog",
-      breed: "Beagle",
-    }),
-    React.createElement(Pet, {
-      name: "Riko",
-      animal: "Dog",
-      breed: "Border Collie",
-    }),
-  ]);
+  return (
+    <div id="something">
+      <h1>Adopt Me!!</h1>
+      <Pet name="Fito" animal="Dog" breed="Afador" />
+      <Pet name="Martin Caballero" animal="Dog" breed="Beagle" />
+      <Pet name="Riko" animal="Dog" breed="Border Collie" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
