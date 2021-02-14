28092c21d49e3485fd4990162a542a6393247dbe
“[Mateo <[lmateo@uninorte.edu.co]>
2021-02-14 08:51:01 +0100

    Class components - Error Boundaries -> Creating the details page

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,55 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
-const Details = () => {
-  return <h1>Hi lol</h1>;
-};
+class Details extends React.Component {
+  // New way of going constructors ->  state = { loading: true };
+  constructor(props) {
+    super(props);
 
-export default Details;
+    this.state = { loading: true };
+  }
+
+  componentDidMount() {
+    //throw new Error()
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    }, console.error);
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+
+    const { animal, breed, location, description, media, name } = this.state;
+
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default function DilsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
